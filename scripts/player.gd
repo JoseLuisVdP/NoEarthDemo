1d9a8b1a4f67d74ee200b3a69e5b7efc8a33e5c4
jose <joluba2002@gmail.com>
2024-03-09 10:46:42 +0100

    init

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody3D
 
+@onready var _mesh = $Mesh
+
 @export var toBeMonitored : Dictionary
 
 var isMoving : bool = false
 var mvmtKeysPressed : int = 0
+var meshRotation : float
+var direction
 
 @export_category("Características")
 @export var SPEED : float = 10.0
@@ -36,7 +40,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -44,9 +48,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)	
 
-	velocity = velocity.rotated(Vector3(0, 1, 0), $Camera/horizontalRotation.rotation.y)
-	$Mesh.rotation = Vector3(0,$Camera/horizontalRotation.rotation.y,0) + Vector3(0, asin(-direction.x), 0)
-	print(direction)
+	velocity = velocity.rotated(Vector3(0, 1, 0), %Camera.get_rotation_y())
 	move_and_slide()
 	
 	# DEBUG
@@ -60,10 +62,12 @@ func _while_idle(delta):
 func _while_walking(delta):
 	if mvmtKeysPressed == 0:
 		%StateMachine.send_event("idle")
+	change_direction()
 
 func _while_running(delta):
 	if mvmtKeysPressed == 0:
 		%StateMachine.send_event("idle")
+	change_direction()
 	
 func _on_start_running():
 	SPEED *= RUN_MULTIPLIER
@@ -79,27 +83,18 @@ func set_monitor_values():
 	toBeMonitored["velocidad-z"] = velocity.z
 	toBeMonitored["camara-x"] = %Camera.get_rotation_degrees_x()
 	toBeMonitored["camara-y"] = %Camera.get_rotation_degrees_y()
-	toBeMonitored["playermesh-x"] = $Mesh.rotation_degrees.x
-	toBeMonitored["playermesh-y"] = $Mesh.rotation_degrees.y
-	toBeMonitored["camara-root-y"] = %Camera.rotation_degrees.y
-
+	toBeMonitored["playermesh-x"] = _mesh.rotation_degrees.x
+	toBeMonitored["playermesh-y"] = _mesh.rotation_degrees.y
 	%VariablesMonitor.args = toBeMonitored
-
 
 func _on_start_moving():
 	isMoving = true
-	
-	#self.rotation_degrees.y += %Camera.get_rotation_degrees_y()
-	#%Camera.yaw = self.rotation_degrees.y
-	#if abs(%Camera.yaw) > 180:
-		#%Camera.yaw -= 360 * sign(%Camera.yaw)
-	#%Camera.set_rotation_degrees_y(0)
-	#%Camera.omitOneFrame = true
-	
 	if Input.is_action_pressed("run"):
 		%StateMachine.send_event("running")
 
 func _on_stop_moving():
-	#%Camera.yaw = 0
-	#%Camera.omitOneFrame = true
 	pass
+
+func change_direction():
+	meshRotation = %Camera.get_rotation_y() + (PI if direction.z > 0 else 0) + asin(-direction.x) * sign(-direction.z+0.5)
+	_mesh.rotation.y = lerp_angle(_mesh.rotation.y, meshRotation, 0.5)
